974b39026c7aff618ac829201904aa402e92cf1f
a.aguri <>
2019-07-20 16:22:49 +0900

    チャット部分の投稿者の判定処理

--- pages/chat/_roomid.vue
+++ pages/chat/_roomid.vue
@@ -4,7 +4,7 @@
       <v-layout row>
         <v-flex xl8>
           <header class="header" v-if="inRoom">
-            <title class="chatTitle">ここにチャットのタイトルを入れるここにチャットのタイトルを入れる</title>
+            <title class="chatTitle">{{title}}</title>
             <v-form ref="form">
               <v-btn key="logout" v-on:click="logout()" v-if="inRoom" class="exitBtn"><v-icon>arrow_back</v-icon></v-btn>
             </v-form>
@@ -21,7 +21,7 @@
               <div
                 v-else
                 :key="'chat'+index"
-                class="chat-item"
+                class="chat-item" :class="[item.show]"
               >
                 <div class="account">
                   <div class="icon">
@@ -30,7 +30,7 @@
                 </div>
                 <div>
                   <div class="author" v-html="item.author"></div>
-                  <div class="speechBubble">
+                  <div class="speechBubble" :class="[item.show]">
                     <div class="message" v-html="item.message"></div>
                   </div>
                   <div class="date" v-html="item.createdAt"></div>
@@ -53,14 +53,13 @@
                 <v-btn small key="image" v-on:click="imgae()" v-if="inRoom" class="ma-0 imageBtn">
                   <v-icon>add_photo_alternate</v-icon>
                 </v-btn>
-                <v-text-field  class="commentInput" key="keyword" v-if="inRoom" v-model="keyword" label="コメント記入"></v-text-field>
+                <v-text-field class="commentInput" key="keyword" single-line outline v-if="inRoom" v-model="keyword" label="コメント記入"></v-text-field>
                 <!-- <v-btn key="talk" v-on:click="submit()" v-if="inRoom" class="commentBtn">
                   <v-icon>send</v-icon>
                 </v-btn> -->
                 <v-btn small key="talk" v-on:click="submit()" v-if="inRoom" class="ma-0 commentBtn">
                   <v-icon>send</v-icon>
                 </v-btn>
-                
               </div>
             </v-form>
           </footer>
@@ -88,6 +87,8 @@ export default {
     items: [],
     members: [],
     roomId: null,
+    title: "",
+    showState: "",
   }),
   watch: {
     inRoom: function(val){
@@ -100,10 +101,25 @@ export default {
             doc.docChanges().forEach(v=>{
               if(v.type == "added"){
                 const data = v.doc.data();
+                // コメントの表示判定
+                // 自分のコメントを右側に表示、他の人のコメントは左側に表示、システム側のコメントは中央に表示
+                console.log(this.name);
+                console.log(data.author);
+                console.log(data.author == this.name);
+                if(this.name == data.author){
+                  this.showState = 'is-mine';
+                }
+                else if(!data.author){
+                  this.showState = 'is-system';
+                }
+                else{
+                  this.showState = 'is-other';
+                }
                 this.items.push({
                   author: data.author,
                   message: data.message,
                   createdAt: String(data.createdAt.toDate().getMonth()+1) + '/' + String(data.createdAt.toDate().getDate()) + '  ' + String(data.createdAt.toDate().toLocaleTimeString()),
+                  show: this.showState,
                 });
               }
             });
@@ -117,7 +133,16 @@ export default {
     await this.getUser();
     this.roomId = this.$route.params.roomid;
     roomRef = db.collection("chats").doc(this.roomId);
-    //this.items.push({message: this.user.uid});
+    // タイトルを取得
+    var getDoc = roomRef.get()
+    .then(doc => {
+      if (doc.exists)  {
+        this.title = doc.data().title;
+      }
+    })
+    .catch(err => {
+      console.log('Error getting document', err);
+    });
     // メンバー更新時
     roomRef.collection("members").onSnapshot(doc=>{
       doc.docChanges().forEach(v=>{
@@ -174,7 +199,7 @@ export default {
 </script>
 
 
-<style lang="css" scoped>
+<style lang="scss" scoped>
 /* ヘッダー */
 .header{
   display: flex;
@@ -206,80 +231,101 @@ export default {
   align-self: center;
 }
 
-/* チャット内 */
 .chat-list{
   padding-top: 40px;
   padding-bottom: 40px;
 }
 
-
+// チャット部分
 .chat-item{
   display: flex;
   margin-bottom: 15px;
-}
 
-.speechBubble{
-  position: relative; /* 三角の位置を固定するために設定 */
-  display: inline-block;
-  max-width: 300px;
-  margin: 0px 20px;
-  padding: 8px 15px; /* ふきだし内の余白 */
-  background: #f0f0f0; /* 背景色 */
-  text-align: left; /* テキストの揃え */
-  border-radius: 15px;
+  .speechBubble{
+    position: relative; /* 三角の位置を固定するために設定 */
+    display: inline-block;
+    max-width: 300px;
+    margin: 0px 20px;
+    padding: 8px 15px; /* ふきだし内の余白 */
+    background: #f0f0f0; /* 背景色 */
+    text-align: left; /* テキストの揃え */
+    border-radius: 15px;
+    flex-direction: row;
 
-  flex-direction: row;
-  
-}
+    &:after {
+      content: '';
+      border: 14px solid transparent;
+      border-top-color: #f0f0f0;
+      position: absolute;
+      top: 0;
+      left: -10px;
+    }
+  }
+  // じぶんの場合
+  &.is-mine{
+    flex-direction: row-reverse;
 
-.speechBubble:after {
-  content: '';
-  border: 14px solid transparent;
-  border-top-color: #f0f0f0;
-  position: absolute;
-  top: 0;
+    .speechBubble::after{
+      right: -10px;
+      left: 10px;
+    }
+  }
 
-  left: -10px;
-}
-.icon{
-  width: 32px;
-  height: 32px;
-  border-radius: 50%;
-  border: 1px solid #555;
-  overflow: hidden;
-  position: relative;
-}
-.iconImage{
-  width: 20px;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  left: 0;
-  margin: auto;
-}
+  .icon{
+    width: 32px;
+    height: 32px;
+    border-radius: 50%;
+    border: 1px solid #555;
+    overflow: hidden;
+    position: relative;
+  }
+  .iconImage{
+    width: 20px;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    margin: auto;
+  }
 
-.message{
-  font-size: 14px;
-  word-break: break-all;
-  line-height: 1.5;
-}
-.author{
-  font-size: 12px;
-  line-height: 18px;
-  min-height: 18px;
-  padding-left: 18px;
-}
-.date{
-  font-size: 10px;
-  padding-left: 20px;
+  .message{
+    font-size: 14px;
+    word-break: break-all;
+    line-height: 1.5;
+  }
+  .author{
+    font-size: 12px;
+    line-height: 18px;
+    min-height: 18px;
+    padding-left: 18px;
+  }
+  .date{
+    font-size: 10px;
+    padding-left: 20px;
+  }
+
+  // システム表示
+  &.is-system{
+    justify-content: center;
+
+    .speechBubble::after{
+      display: none;
+    }
+    .icon{
+      display: none;
+    }
+    .date{
+      display: none;
+    }
+  }
 }
 
 /* フッター */
 .footer{
   width: 100%;
   position: fixed;
-  bottom: -10px;
+  bottom: 0;
   right: 0;
   left: 0;
   background-color: #E4E4E4;
@@ -317,4 +363,10 @@ div.btn__content {
 div.card__actions .btn{
   min-width: 0;
 }
+
+.commentInput{
+  height: 64px;
+  padding-top: 4px;
+}
+
 </style>
