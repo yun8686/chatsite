15bfa35b543941bf1bc58441814832d43dfe908f
a.aguri <>
2019-09-03 23:24:17 +0900

    SEOの対応

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,12 +6,16 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: '%s - オープンチャットサイト-Freeオープンチャット',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: '無料で手軽に作れるレンタルチャットのサイトです。ルームを作成してURLを共有すれば、友人とチャットをすることができます。また合言葉の設定でプライベートチャットをすることも可能です。オープンチャット機能もありますので、タイトルで検索して、共通の話題でチャットも楽しめます。' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'オープンチャットサイト-Freeオープンチャット' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://freeopenchat.com/' },
+      { hid: 'og:title', property: 'og:title', content: 'Freeオープンチャット' },
+      { name: 'keywords', content: 'オープンチャット,無料チャット,フリーチャット,Free Chat,登録不要,検索可能,ルーム作成,URLを共有,プライベートチャット,URLでチャット'},
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
